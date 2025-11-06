9542acc6833dd38eb5a51a889ff5bd16d25b5400
ishaf <shafiulislam20@gmail.com>
2025-11-06 16:20:21 +0600

    feat: update core query classes to use QueryKey
    - Update Query class to accept QueryKey instead of String
    - Maintain backward compatibility with String key property
    - Update QueryClient methods to accept QueryKey
    - Update InfiniteQuery to use QueryKey
    - Update PrefetchConfig to use QueryKey
    - Update QueryBuilder widget to use QueryKey

--- packages/fasq/lib/src/core/prefetch_config.dart
+++ packages/fasq/lib/src/core/prefetch_config.dart
@@ -1,12 +1,13 @@
+import 'query_key.dart';
 import 'query_options.dart';
 
 class PrefetchConfig<T> {
-  final String key;
+  final QueryKey queryKey;
   final Future<T> Function() queryFn;
   final QueryOptions? options;
 
   const PrefetchConfig({
-    required this.key,
+    required this.queryKey,
     required this.queryFn,
     this.options,
   });
