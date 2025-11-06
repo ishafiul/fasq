4c3cc22e6cbc6623e444320412d645c7bc214a6e
ishaf <shafiulislam20@gmail.com>
2025-11-06 16:20:44 +0600

    feat(riverpod): update riverpod package to use QueryKey
    - Update QueryProvider to use QueryKey
    - Update InfiniteQueryProvider to use QueryKey
    - Update QueryNotifier to use QueryKey
    - Update InfiniteQueryNotifier to use QueryKey
    - Update PrefetchExtension to use QueryKey

--- packages/fasq_riverpod/lib/src/query_provider.dart
+++ packages/fasq_riverpod/lib/src/query_provider.dart
@@ -1,13 +1,13 @@
 import 'package:fasq_riverpod/fasq_riverpod.dart';
 
 StateNotifierProvider<QueryNotifier<T>, QueryState<T>> queryProvider<T>(
-  String key,
+  QueryKey queryKey,
   Future<T> Function() queryFn, {
   QueryOptions? options,
 }) {
   return StateNotifierProvider<QueryNotifier<T>, QueryState<T>>((ref) {
     return QueryNotifier<T>(
-      key: key,
+      queryKey: queryKey,
       queryFn: queryFn,
       options: options,
     );
