9bcd6206f16439a0b0ee0d9b908548df148630fd
ishaf <shafiulislam20@gmail.com>
2025-11-26 22:39:43 +0600

    chore: update platform-specific configuration files
    - Update AndroidManifest.xml for Android platform
    - Update GeneratedPluginRegistrant.swift for macOS platform

--- examples/ecommerce/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/ecommerce/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import flutter_secure_storage_macos
+import package_info_plus
 import path_provider_foundation
 import share_plus
 import sqflite_darwin
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
