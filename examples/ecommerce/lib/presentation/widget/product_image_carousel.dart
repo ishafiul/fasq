585dea18c1d251d1ffd58f77389cd111ea9714d1
ishaf <shafiulislam20@gmail.com>
2025-12-02 11:08:23 +0600

    feat(core): Add Swiper and ImageViewer widgets

--- examples/ecommerce/lib/presentation/widget/product_image_carousel.dart
+++ examples/ecommerce/lib/presentation/widget/product_image_carousel.dart
@@ -2,7 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecommerce/api/models/images2.dart';
 import 'package:ecommerce/core/colors.dart';
 import 'package:ecommerce/core/const.dart';
+import 'package:ecommerce/core/widgets/image_viewer/image_viewer.dart';
+import 'package:ecommerce/core/widgets/page_indicator.dart';
 import 'package:ecommerce/core/widgets/spinner/circular_progress.dart';
+import 'package:ecommerce/core/widgets/swiper.dart';
 import 'package:flutter/material.dart';
 
 class ProductImageCarousel extends StatefulWidget {
@@ -18,27 +21,17 @@ class ProductImageCarousel extends StatefulWidget {
 }
 
 class _ProductImageCarouselState extends State<ProductImageCarousel> {
-  final PageController _pageController = PageController();
+  final SwiperRef _swiperRef = SwiperRef();
   int _currentIndex = 0;
 
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  void _onPageChanged(int index) {
+  void _onIndexChange(int index) {
     setState(() {
       _currentIndex = index;
     });
   }
 
   void _onThumbnailTap(int index) {
-    _pageController.animateToPage(
-      index,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
+    _swiperRef.swipeTo(index);
   }
 
   @override
@@ -48,92 +41,108 @@ class _ProductImageCarouselState extends State<ProductImageCarousel> {
     final palette = context.palette;
 
     if (widget.images.isEmpty) {
-      return _ImagePlaceholder();
+      return Container(
+        height: 400,
+        color: palette.surface,
+        child: Center(
+          child: Icon(
+            Icons.image_not_supported_outlined,
+            color: palette.weak,
+            size: spacing.xxl,
+          ),
+        ),
+      );
     }
 
     final sortedImages = List<Images2>.from(widget.images)..sort((a, b) => a.displayOrder.compareTo(b.displayOrder));
 
     return Column(
       children: [
-        SizedBox(
-          height: 400,
-          child: PageView.builder(
-            controller: _pageController,
-            onPageChanged: _onPageChanged,
-            itemCount: sortedImages.length,
-            itemBuilder: (context, index) {
-              final image = sortedImages[index];
-              return _ImageItem(imageUrl: image.url);
-            },
+        GestureDetector(
+          onTap: () {
+            showMultiImageViewer(
+              context,
+              MultiImageViewerProps(
+                images: sortedImages.map((img) => img.url).toList(),
+                defaultIndex: _currentIndex,
+              ),
+            );
+          },
+          child: SizedBox(
+            height: 400,
+            child: Swiper(
+              ref: _swiperRef,
+              defaultIndex: _currentIndex,
+              onIndexChange: _onIndexChange,
+              showIndicator: sortedImages.length > 1,
+              indicatorProps: const SwiperIndicatorProps(
+                color: PageIndicatorColor.primary,
+                position: SwiperIndicatorPosition.center,
+              ),
+              children: sortedImages
+                  .map((image) => SwiperItem(
+                        child: CachedNetworkImage(
+                          imageUrl: image.url,
+                          fit: BoxFit.contain,
+                          placeholder: (context, url) => ColoredBox(
+                            color: palette.surface,
+                            child: Center(
+                              child: CircularProgressSpinner(color: palette.brand, size: 24, strokeWidth: 2),
+                            ),
+                          ),
+                          errorWidget: (context, url, error) => ColoredBox(
+                            color: palette.surface,
+                            child: Icon(
+                              Icons.image_not_supported_outlined,
+                              color: palette.weak,
+                              size: spacing.lg,
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
           ),
         ),
         if (sortedImages.length > 1) ...[
           SizedBox(height: spacing.sm),
           SizedBox(
             height: 60,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: sortedImages.length,
-              itemBuilder: (context, index) {
-                final image = sortedImages[index];
-                final isSelected = index == _currentIndex;
-                return GestureDetector(
-                  onTap: () => _onThumbnailTap(index),
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    margin: EdgeInsets.only(right: index < sortedImages.length - 1 ? spacing.xs : 0),
-                    decoration: BoxDecoration(
-                      borderRadius: radius.all(radius.sm),
-                      border: Border.all(
-                        color: isSelected ? palette.brand : palette.border,
-                        width: isSelected ? 2 : 1,
+            child: Center(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: sortedImages.asMap().entries.map((entry) {
+                    final index = entry.key;
+                    final image = entry.value;
+                    final isSelected = index == _currentIndex;
+                    return GestureDetector(
+                      onTap: () => _onThumbnailTap(index),
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        margin: EdgeInsets.only(right: index < sortedImages.length - 1 ? spacing.xs : 0),
+                        decoration: BoxDecoration(
+                          borderRadius: radius.all(radius.sm),
+                          border: Border.all(
+                            color: isSelected ? palette.brand : palette.border,
+                            width: isSelected ? 2 : 1,
+                          ),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: radius.all(radius.sm),
+                          child: _ThumbnailImage(imageUrl: image.url),
+                        ),
                       ),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: radius.all(radius.sm),
-                      child: _ThumbnailImage(imageUrl: image.url),
-                    ),
-                  ),
-                );
-              },
+                    );
+                  }).toList(),
+                ),
+              ),
             ),
           ),
         ],
       ],
-    );
-  }
-}
-
-class _ImageItem extends StatelessWidget {
-  const _ImageItem({
-    required this.imageUrl,
-  });
-
-  final String imageUrl;
-
-  @override
-  Widget build(BuildContext context) {
-    final palette = context.palette;
-    final spacing = context.spacing;
-
-    return CachedNetworkImage(
-      imageUrl: imageUrl,
-      fit: BoxFit.contain,
-      placeholder: (context, url) => ColoredBox(
-        color: palette.surface,
-        child: Center(
-          child: CircularProgressSpinner(color: palette.brand, size: 24, strokeWidth: 2),
-        ),
-      ),
-      errorWidget: (context, url, error) => ColoredBox(
-        color: palette.surface,
-        child: Icon(
-          Icons.image_not_supported_outlined,
-          color: palette.weak,
-          size: spacing.lg,
-        ),
-      ),
     );
   }
 }
@@ -170,25 +179,3 @@ class _ThumbnailImage extends StatelessWidget {
     );
   }
 }
-
-class _ImagePlaceholder extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final palette = context.palette;
-    final spacing = context.spacing;
-
-    return Container(
-      height: 400,
-      color: palette.surface,
-      child: Center(
-        child: Icon(
-          Icons.image_not_supported_outlined,
-          color: palette.weak,
-          size: spacing.xxl,
-        ),
-      ),
-    );
-  }
-}
-
-
